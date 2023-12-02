6d79d4221a6ecd5ec313b1dc0a96fb0eec767111
Rui Cruz <rpsc16@gmail.com>
2023-12-02 13:54:08 +0000

    Defeat Menu added

---
+++ src/main/java/com/aor/spaceship/controller/menu/DefeatMenuController.java
@@ -0,0 +1,28 @@
+package com.aor.spaceship.controller.menu;
+
+import com.aor.spaceship.Application;
+import com.aor.spaceship.controller.Controller;
+import com.aor.spaceship.gui.GUI;
+import com.aor.spaceship.model.game.arena.ArenaBuilder;
+import com.aor.spaceship.model.menu.DefeatMenu;
+import com.aor.spaceship.states.GameState;
+
+public class DefeatMenuController extends Controller<DefeatMenu> {
+    public DefeatMenuController(DefeatMenu defeatMenu) { super(defeatMenu); }
+
+    @Override
+    public void step(Application application, GUI.Action action, long time) {
+        switch (action) {
+            case UP:
+                getModel().previousEntry();
+                break;
+            case DOWN:
+                getModel().nextEntry();
+                break;
+            case SELECT:
+                if (getModel().isSelectedExit()) application.setState(null);
+                if (getModel().isSelectedStart()) application.setState(new GameState(new ArenaBuilder(30, 30).createArena()));
+        }
+    }
+
+}
