9b93dfab13c4bcfa3a882b4b4df6a1b506c0ad42
Rui Cruz <rpsc16@gmail.com>
2023-12-08 22:14:06 +0000

    Changed some things in the Instructions Menu

--- src/main/java/com/aor/spaceship/viewer/menu/InstructionsMenuViewer.java
+++ src/main/java/com/aor/spaceship/viewer/menu/InstructionsMenuViewer.java
@@ -13,5 +13,25 @@ public class InstructionsMenuViewer extends Viewer<InstructionsMenu> {
     protected void drawElements(Application application, GUI gui) {
         gui.drawText(new Position(26, 1), getModel().getEntry(0), getModel().isSelected(0) ? "#00FF00" : "#FFFFFF");
         gui.drawText(new Position(28, 1), getModel().getEntry(1), getModel().isSelected(1) ? "#FF0000" : "#FFFFFF");
+        gui.drawText(new Position(1,1), "Controls: ", "#89CFF0");
+        gui.drawText(new Position(1,3), "↑", "#89CFF0");
+        gui.drawText(new Position(2,3), " : Move Up", "#FFFFFF");
+        gui.drawText(new Position(1,5), "↓", "#89CFF0");
+        gui.drawText(new Position(2,5), " : Move Down", "#FFFFFF");
+        gui.drawText(new Position(1,7), "←", "#89CFF0");
+        gui.drawText(new Position(2,7), " : Move Left", "#FFFFFF");
+        gui.drawText(new Position(1,9), "→", "#89CFF0");
+        gui.drawText(new Position(2,9), " : Move Right", "#FFFFFF");
+        gui.drawText(new Position(1,15), "Elements: ", "#89CFF0");
+        gui.drawText(new Position(1,17), "»", "#FFFFFF");
+        gui.drawText(new Position(2,17), " : Our Spaceship", "#FFFFFF");
+        gui.drawText(new Position(1,19), "Ó", "#FF0000");
+        gui.drawText(new Position(2,19), " : Meteors", "#FFFFFF");
+        gui.drawText(new Position(1,21), "Ú", "#FFC300");
+        gui.drawText(new Position(2,21), " : Default Enemies", "#FFFFFF");
+        gui.drawText(new Position(1,23), "Û", "#FF0FF0");
+        gui.drawText(new Position(2,23), " : Special Enemies", "#FFFFFF");
+        gui.drawText(new Position(1,25), "Ù", "#FFFF00");
+        gui.drawText(new Position(2,25), " : Powers", "#FFFFFF");
     }
 }
